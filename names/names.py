09cf87c08e02362a8f824c1f31edbc928d3dd15a
Shrey <ShreyasJothish>
2019-06-14 10:12:17 -0700

    Enabled old code for comparison

--- names/names.py
+++ names/names.py
@@ -1,7 +1,6 @@
 import time
 from binary_search_tree import BinarySearchTree
 
-start_time = time.time()
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -10,14 +9,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-"""
+start_time = time.time()
+
 duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
-"""
 
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+print(f'\nOptimized solution using BST\n')
+start_time = time.time()
 # Use BinarySearchTree to add names from names_1
 # and compare the duplicates using names_2
 duplicates = []
