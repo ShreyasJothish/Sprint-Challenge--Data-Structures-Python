a3b2aea56ec231f79caa205ec2d26850d3eb2b0f
Shrey <ShreyasJothish>
2019-06-14 08:18:00 -0700

    ring_buffer implementation completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,23 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        # Insert at current location and increment
+        self.storage[self.current] = item
+        self.current += 1
 
-  def get(self):
-    pass
+        # Handle roll over case
+        if self.current == self.capacity:
+            self.current = 0
+
+    def get(self):
+        output = []
+        # Start fetching the items from current location
+        for i in range(self.capacity):
+            if self.storage[i]:
+                output.append(self.storage[i])
+
+        return output
